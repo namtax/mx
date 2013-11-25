f4372ad42d9cdf8c59cb86e45d4b4329fc61bdf1
namtax <namtax@googlemail.com>
2013-11-25 10:13:56 +0000

    add stylus

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var jade = require('jade');
 var app = express();
+var path = require('path');
 var SongmeaningService = require('./songmeaning_service').SongmeaningService;
 
 app.set('views',__dirname + '/views');
@@ -8,6 +9,8 @@ app.set('view engine', 'jade');
 
 app.configure(function(){ 
   app.use(express.bodyParser());
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 var songmeaningService = new SongmeaningService('localhost', 27017);
